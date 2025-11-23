d9f575a073eb3ffdb11b50b6621dab67ee16a73e
Gal Helner <galhelner@gmail.com>
2025-11-23 10:11:43 +0200

    added simple home page component to the dashboard

---
+++ frontend/src/app/home/pages/home-page/home-page.component/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'home-page',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css',
+})
+export class HomePageComponent {
+
+}
